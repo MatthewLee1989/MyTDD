120eff6c1d4a1652e5fcfc92cbcc5c66fd42812c
李梓铭 <liziming@cvte.com>
2017-08-31 17:55:33 +0800

    Add some function test code.

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.middleware.csrf import get_token
 
 from lists.views import home_page
 
@@ -11,8 +12,28 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
     
-    def test_home_page_returns_correct_html(self):
+    # def test_home_page_returns_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     expected_html = render_to_string('home.html')
+    #     self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        self.assertIn('A new list item', response.content.decode())
+        csrf_token_value = get_token(request)
+        expected_html = render_to_string(
+            'home.html',
+            {
+                'new_item_text': 'A new list item',
+                'csrf_token_value': csrf_token_value
+            }
+        )
+        print('response:  ' + response.content.decode())
+        print('expected_html:   ' + expected_html)
+        print(csrf_token_value)
         self.assertEqual(response.content.decode(), expected_html)
